a7f4eb9d9eba0e390dc7449ab98193262f60d67f
Santiago <santiago.bastidascaro@gmail.com>
2024-08-13 15:56:01 -0500

    Correccion de errores, los cuales afectaban el codigo, backend totalmente limpio

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const express = require("express")
 const app = express();
 const cors = require('cors')
-const { rateLimit } = require('express-rate-limit')
+
 const api_routes = require("./routes/index");
 
-const limiter = rateLimit({
-    windowMs:5 * 60 * 1000,
-    limit: 15,
-    message: {
-        ok: false,
-        message: 'Ha excedido el numero de peticiones'
-    }
-})
-
-
-app.use(limiter)
 
 app.use(express.static('public'));
 app.use(cors())
